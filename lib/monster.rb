c43ba5a970bc2d074013058024090fdde9366f74
Nitrosito <nitrosito@gmail.com>
2015-10-25 22:02:06 +0100

    Creado monstruo de prueba, fix bad initialize sintax

--- lib/monster.rb
+++ lib/monster.rb
@@ -1,13 +1,15 @@
 # To change this license header, choose License Headers in Project Properties.
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
-
+  require_relative "prize.rb"
+  require_relative "bad_consequence.rb"
+  require_relative "treasure_kind.rb"
 class Monster
-  def initialize(name, combatLevel, prize, badConsecuence )
+  def initialize(name, combatLevel, prize, badConsequence )
     @name = name
     @combatLevel = combatLevel
     @prize = prize
-    @badconsecuence = badConsecuence
+    @badconsequence = badConsequence
   end
   
   attr_accessor :name, :combatLevel
@@ -15,7 +17,17 @@ class Monster
   def to_s
     "Nombre Monstruo: #{name} \n
            Nivel Combate: #{combatLevel}\n
-           Buen Rollo: #{prize}\n
-           Mal rollo: #{badconsequence.to_s}"
+           Buen Rollo: #{prize.to_s}\n
+           Mal rollo: #{badconsequence.to_s}" 
   end
 end
+
+#Creando un monstruo #FIXME #TODO
+ pChibithulhu = Prize.new(1,1)
+ bcChibithulhu = BadConsequence.newLevelSpecificTreasures('Embobados con el
+ lindo primigenio te descartas de tu casco visible', 0, 
+ [TreasureKind::BOTHHANDS], nil)
+ mimonstruo = Monster.new('Chibithulhu',2,pChibithulhu,bcChibithulhu)
+ 
+puts mimonstruo.to_s
+ 
