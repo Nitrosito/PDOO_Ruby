eb45bebbaae1d5d0a2bc2a11fa1bce99d52e985c
Nitrosito <nitrosito@gmail.com>
2015-10-25 19:48:42 +0100

    Probando clases y accesos a variables de clase

---
+++ lib/prueba_napakalaki.rb
@@ -0,0 +1,20 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+  require_relative "prize.rb"
+  require_relative "bad_consequence.rb"
+  require_relative "monster.rb"
+  require_relative "treasure_kind.rb"
+class PruebaNapakalaki
+  def initialize
+    
+  end
+  
+end
+
+  # Probando a crear objetos de las clases creadas y consultarlos
+  mimonstruo = Monster.new("Tux", 100, Prize.new(10, 10), BadConsequence.new("meh",3,3,3,3,3,false)) # Creo objeto monstruo
+  puts mimonstruo.name #nombre
+  puts mimonstruo.combatLevel #Nivel
+  
+
