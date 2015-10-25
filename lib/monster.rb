de3cc6e219fca77d25a36fac7fa5df079e619db1
Nitrosito <nitrosito@gmail.com>
2015-10-25 22:11:27 +0100

    Fix format to_s Monstruo

--- lib/monster.rb
+++ lib/monster.rb
@@ -15,9 +15,9 @@ class Monster
   attr_accessor :name, :combatLevel 
   
   def to_s
-    "Nombre Monstruo: #{@name} \n
-           Nivel Combate: #{@combatLevel}\n
-           Buen Rollo: #{@prize.to_s}\n
+    "Nombre Monstruo: #{@name} 
+           Nivel Combate: #{@combatLevel}
+           Buen Rollo: #{@prize.to_s}
            Mal rollo: #{@badconsequence.to_s}" 
   end
 end
