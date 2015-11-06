eb52099614492772e3cce763f43f710a1da5f738
nicolasrhc <nicolasrhc@gmail.com>
2015-11-06 12:28:18 +0100

    añadida clase Napakalaki en ruby :penguin:

---
+++ lib/napakalaki.rb
@@ -0,0 +1,68 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+class Napakalaki
+  include singleton
+  
+  def initialize
+    @currentlayer
+    @players
+    @dealer
+    @currentMonster
+  end
+  
+  def initPlayers(names)
+    
+  end
+  
+  def nextPlayer
+    
+  end
+  
+  def nextTurnAllowed
+    
+  end
+  
+  def setEnemies
+    
+  end
+  
+  public
+  
+  def developCombat
+    
+  end
+  
+  def discardVisibleTreasures(treasures)
+    
+  end
+  
+  def discardHiddenTreasures(treasures)
+    
+  end
+  
+  def makeTreasuresVisible(treasures)
+    
+  end
+  
+  def initGame(players)
+    
+  end
+  
+  def getCurrentPlayer
+    
+  end
+  
+  def getCurrentMonster
+    
+  end
+  
+  def nextTurn
+    
+  end
+  
+  def endOfGame(result)
+    
+  end
+end
