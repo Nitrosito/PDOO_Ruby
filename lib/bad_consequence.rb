c05936a248e771ab9bcc842ed7532614afdbe384
Nitrosito <nitrosito@gmail.com>
2015-11-10 20:20:41 +0100

    Añadida contanste MAX, constructor muerte

--- lib/bad_consequence.rb
+++ lib/bad_consequence.rb
@@ -3,6 +3,7 @@
 # and open the template in the editor.
 
 class BadConsequence
+  @@MAXTREASURES=10
   def initialize(aText, someLevels, someVisibleTreasures, someHiddenTreasures, someSpecificVisibleTreasures, someSpecificHiddenTreasures, death)
     @text = aText
     @levels = someLevels
@@ -24,8 +25,8 @@ class BadConsequence
     new(aText,someLevels,0,0,someSpecificVisibleTreasures,someSpecificHiddenTreasures,false)
   end
   
-  def self.newDeath(aText)
-    new(aText,0,0,0,nil,nil,true)
+  def self.newDeath(aText,death)
+    new(aText,0,0,0,nil,nil,death)
   end
   
   private_class_method:new ##FIXME Esto va aqui o delante de todos los def de los metodos new*?
