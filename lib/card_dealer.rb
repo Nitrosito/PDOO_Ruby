565e75bc3bc0c863da6407f831e4f09f7d35f3b2
Nitrosito <nitrosito@gmail.com>
2015-11-12 19:41:24 +0100

    Fix bad requite Singleton

--- lib/card_dealer.rb
+++ lib/card_dealer.rb
@@ -1,9 +1,11 @@
+#encoding: utf-8
 # To change this license header, choose License Headers in Project Properties.
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
+require 'singleton'
 
 class CardDealer
-  include singleton
+  include Singleton
   
   def initializer
     @unusedMonster
