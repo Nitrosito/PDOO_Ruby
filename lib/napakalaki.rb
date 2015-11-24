a7c93bf0604825176bdf639b0cc010bf767cec79
Nitrosito <nitrosito@gmail.com>
2015-11-25 00:58:21 +0100

    Next Player y nextTurnIsAllowed

--- lib/napakalaki.rb
+++ lib/napakalaki.rb
@@ -1,27 +1,45 @@
 # To change this license header, choose License Headers in Project Properties.
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
-
+require 'singleton'
+require 'card_dealer.rb'
+require 'player.rb'
 class Napakalaki
-  include singleton
+  include Singleton
   
   def initialize
-    @currentlayer
-    @players
-    @dealer
-    @currentMonster
+    @currentPlayer=nil
+    @players=Array.new
+    @dealer=Cardealer.instance
+    @currentMonster=nil
   end
   
   def initPlayers(names)
-    
+    names.each do |i|
+      @players << Player.new(i);
+    end
   end
   
   def nextPlayer
+    if(@currentPlayer==nil) 
+      aleatorio = rand(@players.size())
+      return @players.at(aleatorio)
+    end
     
+    if(@players.index(@currentPlayer)==@players.size()-1)
+      return @players.at(0);      
+    end
+    
+    actual = @players.index(@currentPlayer)
+    return @players.at(actual+1);
   end
   
-  def nextTurnAllowed
+  def nextTurnIsAllowed
+    if(@currentPlayer==nil)
+        return false
+    end
     
+    return @currentPlayer.validState()
   end
   
   def setEnemies
