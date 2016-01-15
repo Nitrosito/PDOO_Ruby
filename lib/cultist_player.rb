f486dfbd96612d33f0dc718df32c89557062af8c
nitrosito <nitrosito@gmail.com>
2016-01-15 11:52:54 +0100

    Arreglados fallos examen

--- lib/cultist_player.rb
+++ lib/cultist_player.rb
@@ -1,11 +1,14 @@
 # To change this license header, choose License Headers in Project Properties.
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
+require_relative "player.rb"
 module NapakalakiGame
-class CultistPlayer < Player
-  def initialize(enemy,myCultistCard)
-    super(enemy)
-    @totalCultistPlayers =0
+class CultistPlayer 
+      @@totalCultistPlayers =0
+      
+  def initialize(player,myCultistCard)
+    Player.newCopia(player)
+    @@totalCultistPlayers++
     @myCultistCard = myCultistCard
   end
   
--- lib/specific_b_c.rb
+++ lib/specific_b_c.rb
@@ -1,6 +1,7 @@
 # To change this license header, choose License Headers in Project Properties.
 # To change this template file, choose Tools | Templates
 # and open the template in the editor.
+require_relative "bad_consequence.rb"
 module NapakalakiGame
 class SpecificBC < BadConsequence
   def initialize(aText,someLevels,someSpecificVisibleTreasures,someSpecificHiddenTreasures)
